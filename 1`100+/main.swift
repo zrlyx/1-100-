2b5c07038a5cfa1aac0b971c27b248c38a27192b
zrlyx <20141105042hyb@admindeimac-007.local>
2016-06-20 10:20:14 +0800

    Initial Commit

---
+++ 1`100+/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1`100+
+//
+//  Created by  20141105042hyb on 16/6/20.
+//  Copyright © 2016年 20141105042HYB. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
